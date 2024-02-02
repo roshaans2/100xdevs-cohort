6c393c4c64270c73b581dbd1ca57a99219ec48e4
roshaans2 <roshaansathya@gmail.com>
2024-02-02 21:39:37 +0530

    Recoil

---
+++ Week-7/Recoil/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { CountContext } from './context'
+import { useContext } from 'react'
+import {RecoilRoot, useRecoilState, useRecoilValue} from "recoil"
+import { countAtom } from './store/atoms/count'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <RecoilRoot>
+          <Count/>
+      </RecoilRoot>
+    </div>
+  )
+}
+
+function Count({setCount}){
+  return(
+    <div>
+      <CountRender/>
+      <Buttons/>
+    </div>  
+  )
+}
+
+function CountRender(){
+  const count = useRecoilValue(countAtom)
+  return(
+    <div>{count}</div>
+  )
+}
+
+function Buttons(){
+  const [count,setCount] = useRecoilState(countAtom)
+  return(
+    <div>
+      <button onClick={()=>{
+        setCount(count+1)
+      }}>Increment</button>
+      <button onClick={()=>{
+        setCount(count-1)
+      }}>Decrement</button>
+    </div>
+  )
+}
+
+export default App
